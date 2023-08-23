1da0051a6ca5b7d067d7170e31354ef757af2fed
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-23 12:44:29 -0500

    Lecture Demo

--- day_3/demo/app.js
+++ day_3/demo/app.js
@@ -16,5 +16,31 @@ nunjucks.configure('views', {
     express: app
 })
 
+app.get('/', (req, res) => {
+    res.render('home.html.njk', {
+        username: req.session.username
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.html.njk')
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    console.log(req.session)
+    req.session.username = req.body.username
+    res.redirect('/')
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
+
 
 app.listen(4000, () => console.log('Running on port 4000!'))
