c2c20de7c8d7c8be0c1cc1e59ec66546addb93ce
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-24 13:09:18 -0500

    Demo: APIs, AJAX, Axios

--- day_4/demo/app.js
+++ day_4/demo/app.js
@@ -13,6 +13,18 @@ app.get('/', (req, res) => {
     res.sendFile('index.html',{ root: '.'})
 })
 
+app.post('/order-cookies', (req, res) => {
+    console.log(req.body)
+    let totalPrice
+    if (req.body.cookieType === 'Snickerdoodle'){
+        totalPrice = req.body.qty * 2.25
+    } else {
+        totalPrice = req.body.qty * 3
+    }
+
+    res.json({msg: `Your Order is ${totalPrice}`, totalPrice: totalPrice})
+})
+
 
 
 viteExpress.listen(app, port, () => {
