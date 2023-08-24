7676a51397023ef59513087edecf01e918aafc70
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-24 10:55:32 -0500

    Modules & Packages

---
+++ day_4/demo/json-demo/itunes_json.js
@@ -0,0 +1,7 @@
+import mountainSongs from './mountain.json' assert { type: 'json' }
+
+console.log(Object.keys(mountainSongs))
+console.log(mountainSongs(['results'][0].artistName))
+
+const firstSong = mountainSongs['results'][0]
+console.log(Object.keys(firstSong))
