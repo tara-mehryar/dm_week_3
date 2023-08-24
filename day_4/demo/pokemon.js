132ff49177affd9802abea7f51baaab1a9519e43
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-24 12:30:56 -0500

    Demo: APIs, AJAX, Axios

--- day_4/demo/pokemon.js
+++ day_4/demo/pokemon.js
@@ -1,10 +1,23 @@
-import axios from "axios";
+// import axios from "axios";
 
-let getPokemon = () => {
-    const numPokemon = document.querySelector('#num-pokemon').value 
-    const url = 'http://pokeapi.co/api/v2/pokemon?limit=' + numPokemon
+// let getPokemon = () => {
+//     const numPokemon = document.querySelector('#num-pokemon').value 
+//     const url = 'http://pokeapi.co/api/v2/pokemon?limit=' + numPokemon
 
-    console.log(url)
-}
+//     axios.get(url)
+//         .then(res => {
+//             let pokemonList = ''
+//             console.log(res.data.results)
+//             for (const pokemon of res.data.results){
+//                 pokemonList += `<li>${pokemon.name}</li>`
+//             }
+//             console.log(pokemonList)
+//             document.querySelector('#pokemon-list').innerHTML = pokemonList
+//         })
+//         .catch( err => console.log(err))
 
-const 
+//     console.log(url)
+// }
+
+// const button = document.querySelector('#get-pokemon')
+// button.addEventListener('click', getPokemon)
